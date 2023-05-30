d552cc726e046139d1e216790ea779503e81bdc4
Mujiburrohman <muji.official06@gmail.com>
2023-05-31 00:13:36 +0700

    color pallete initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#fff",
+        primary: "#9376E0",
+        "primary-focus": "#9376E0",
+        "primary-content": "#ffffff",
+        secondary: "#E893CF",
+        accent: "#F3BCC8",
+        success: "#00C897",
+        neutral: "#2D2727",
+        warning: "#FFD365",
+        error: "#FF6D60",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
