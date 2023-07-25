9a1b7ceba2d05f390a29e83cae0395fd59ec9a0a
mujibur-rohman <muji.official06@gmail.com>
2023-07-26 00:47:50 +0700

    cart product

--- src/pages/product/[productId]/index.jsx
+++ src/pages/product/[productId]/index.jsx
@@ -1,11 +1,21 @@
 import ProductAPI from "@/network/features/product.api";
 import { getSession, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 function ProductDetail({ product }) {
   const router = useRouter();
   const session = useSession();
-  console.log(session);
+  const [cart, setCart] = useState();
+  console.log(cart);
+
+  useEffect(() => {
+    const carts = JSON.parse(localStorage.getItem("cart"));
+    const mapCart = carts?.map((item) => item.id);
+    setCart(mapCart);
+    console.log(mapCart?.find((e) => e === 15));
+  }, []);
+
   return (
     <section className="py-5 w-[50vw] mx-auto flex gap-3">
       <div className="border-[1px] grow rounded-lg p-3 flex gap-3 self-start">
@@ -30,9 +40,44 @@ function ProductDetail({ product }) {
             <p>Total</p>
             <h1 className="font-bold text-2xl">Rp. {product.price}</h1>
           </div>
-          <button className="text-sm bg-warning transition-colors hover:bg-warning-focus py-1 text-white rounded-md">
-            Tambah Keranjang
-          </button>
+          {cart?.find((e) => e === product.id) ? (
+            <button
+              onClick={() => {
+                const oldCart = JSON.parse(localStorage.getItem("cart"));
+                const filterCart = oldCart.filter(
+                  (item) => item.id !== product.id
+                );
+                const mapCart = filterCart?.map((item) => item.id);
+                setCart(mapCart);
+                localStorage.setItem("cart", JSON.stringify(filterCart));
+              }}
+              className="text-sm bg-error transition-colors hover:bg-warning-focus py-1 text-white rounded-md"
+            >
+              Hapus Keranjang
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                const oldCart = JSON.parse(localStorage.getItem("cart"));
+                let arrCart = [];
+                if (oldCart) {
+                  arrCart.push(...oldCart);
+                  arrCart.push(product);
+                  const mapCart = arrCart?.map((item) => item.id);
+                  setCart(mapCart);
+                  localStorage.setItem("cart", JSON.stringify(arrCart));
+                } else {
+                  arrCart.push(product);
+                  const mapCart = arrCart?.map((item) => item.id);
+                  setCart(mapCart);
+                  localStorage.setItem("cart", JSON.stringify(arrCart));
+                }
+              }}
+              className="text-sm bg-warning transition-colors hover:bg-warning-focus py-1 text-white rounded-md"
+            >
+              Tambah Keranjang
+            </button>
+          )}
           {session?.data?.user ? (
             <button
               onClick={() => router.push(`/checkout/${product.id}`)}
